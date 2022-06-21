9a5cdb36258cda6d52787125bf246beb1a210c2a
Sumonsnote <sohankhance@gmail.com>
2022-06-21 23:38:57 +0600

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "cupcake",
+    ],
   },
   plugins: [require("daisyui")],
 }
