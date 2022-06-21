77c6fb5f545508869168c4e9bc541b73a877f83a
Sumonsnote <sohankhance@gmail.com>
2022-06-22 00:59:06 +0600

    banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import Navbar from './Pages/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import About from './Pages/About';
+import ContactUs from './Pages/ContactUs';
+import Appointment from './Pages/Appointment';
+import Reviews from './Pages/Reviews';
+import Login from './Pages/Login';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="px-12">
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/contactUs' element={<ContactUs></ContactUs>}></Route>
+        <Route path='/appointment' element={<Appointment></Appointment>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
